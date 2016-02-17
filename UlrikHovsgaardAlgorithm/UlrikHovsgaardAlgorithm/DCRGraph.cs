2fd40dbfefa960a7624d5a3efb7f11e2d24ed52e
Paw Høvsgaard <hovsgaardfilm@youseeme.dk>
2016-02-17 10:36:45 +0100

    setting up data structures for testing

--- UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithm/DCRGraph.cs
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithm/DCRGraph.cs
@@ -8,7 +8,22 @@ namespace UlrikHovsgaardAlgorithm
 {
     public class DCRGraph
     {
-        List<int> ints;
+        HashSet<Activity> Activities;
+        Dictionary<Activity, Set<Activity>> Responses;
+        Map<Activity, Map<Activity, bool>> InclusionsExclusions;
+        Map<Activity, Set<Activity>> Conditions;
+        Map<Activity, Set<Activity>> Milestones;
+        Map<Activity, Map<Activity, TimeSpan>> Deadlines;
 
+private class Activity
+        {
+            int Id;
+            string Name;
+            bool Included;
+            bool Executed;
+            bool Pending;
+
+
+        }
     }
 }
