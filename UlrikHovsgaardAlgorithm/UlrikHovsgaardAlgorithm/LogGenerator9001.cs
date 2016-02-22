43f586469f1436f8cc6fdf835f842deb52bbf227
Paw Høvsgaard <hovsgaardfilm@youseeme.dk>
2016-02-22 12:16:57 +0100

    LogGenerator start

---
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithm/LogGenerator9001.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UlrikHovsgaardAlgorithm
+{
+    public class LogGenerator9001
+    {
+        // from 0-100. Default 40. determines the chance that the trace will terminate, when possible. a lower index leads to longer traces.
+        private int terminationIndex = 40;
+        //how many traces to generate
+        private int noOfTraces;
+        
+
+        public LogGenerator9001 (int index, DcrGraph inputGraph)
+        {
+            terminationIndex = index;
+        }
+
+
+        public List<LogEvent> traceGenerator()
+        {
+            throw new NotImplementedException();    
+        } 
+
+
+
+    }
+}
