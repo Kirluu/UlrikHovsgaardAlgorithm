01d0c6409086c59e8d6ac8fa01d2068d7715ace1
Kirluu <kirluu4thewin@gmail.com>
2016-04-06 20:38:11 +0200

    untested additions to imageretriever

--- UlrikHovsgaardAlgorithm/UlrikHovsgaardWpf/GraphImageRetriever.cs
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardWpf/GraphImageRetriever.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Controls;
 using UlrikHovsgaardAlgorithm.Data;
 
 namespace UlrikHovsgaardWpf
 {
-    public class GraphImageRetriever
+    public static class GraphImageRetriever
     {
-        private string _accessString = "http://dcr.itu.dk:8023/trace/render";
+        private static string _accessString = "http://dcr.itu.dk:8023/trace/render";
 
-        public Image Retrieve(DcrGraph graph)
+        public static Image Retrieve(DcrGraph graph)
         {
-            throw new NotImplementedException();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_accessString);
+            request.Method = "POST";
+            request.AllowAutoRedirect = false;
+            //request.CookieContainer = my_cookie_container;
+            //request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"; // TODO: wut
+            request.Accept = "svg+xml";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            string strNew = "source=" + HttpUtility.UrlEncode(graph.ExportToXml());
+
+            using (StreamWriter stOut = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII))
+            {
+                stOut.Write(strNew);
+                stOut.Close();
+            }
+
+            WebResponse response = request.GetResponse();
+            var lala = response.ContentLength;
+
+
+
+            return null;
         }
     }
 }
