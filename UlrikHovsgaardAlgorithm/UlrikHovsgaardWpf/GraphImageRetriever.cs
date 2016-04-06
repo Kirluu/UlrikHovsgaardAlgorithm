004ca6dfea3c816aae57384ad3fc85e099d06752
Kirluu <kirluu4thewin@gmail.com>
2016-04-06 20:06:43 +0200

    Created GraphImageRetriever class

---
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardWpf/GraphImageRetriever.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using UlrikHovsgaardAlgorithm.Data;
+
+namespace UlrikHovsgaardWpf
+{
+    public class GraphImageRetriever
+    {
+        private string _accessString = "http://dcr.itu.dk:8023/trace/render";
+
+        public Image Retrieve(DcrGraph graph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
