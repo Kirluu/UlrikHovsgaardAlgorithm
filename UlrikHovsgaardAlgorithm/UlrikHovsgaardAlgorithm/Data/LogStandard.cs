59d5ebe17fc1f6184c082178e627fc36e6323db7
Kirluu <kirluu4thewin@gmail.com>
2016-04-20 13:53:34 +0200

    Tried my hardest to make log parsing work entirely, but dedded

--- UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithm/Data/LogStandard.cs
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithm/Data/LogStandard.cs
@@ -6,17 +6,31 @@ using System.Threading.Tasks;
 
 namespace UlrikHovsgaardAlgorithm.Data
 {
+    public enum DataType { Int, String }
+
+    public class LogStandardEntry
+    {
+        public DataType DataType { get; set; }
+        public string Name { get; set; }
+
+        public LogStandardEntry(DataType type, string name)
+        {
+            DataType = type;
+            Name = name;
+        }
+    }
+
     public class LogStandard
     {
         public string Namespace { get; set; }
         public string TraceIdentifier { get; set; }
-        public string TraceIdIdentifier { get; set; }
+        public LogStandardEntry TraceIdIdentifier { get; set; }
         public string EventIdentifier { get; set; }
-        public string EventIdIdentifier { get; set; }
-        public string EventNameIdentifier { get; set; }
+        public LogStandardEntry EventIdIdentifier { get; set; }
+        public LogStandardEntry EventNameIdentifier { get; set; }
 
-        public LogStandard(string @namespace, string traceIdentifier, string traceIdIdentifier, string eventIdentifier,
-            string eventIdIdentifier, string eventNameIdentifier)
+        public LogStandard(string @namespace, string traceIdentifier, LogStandardEntry traceIdIdentifier, string eventIdentifier,
+            LogStandardEntry eventIdIdentifier, LogStandardEntry eventNameIdentifier)
         {
             Namespace = @namespace;
             TraceIdentifier = traceIdentifier;
