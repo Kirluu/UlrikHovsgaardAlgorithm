365f56757db813bb80247d0c6c60e00ab49d5778
Kirluu <kirluu4thewin@gmail.com>
2016-04-23 12:01:53 +0200

    Re-added LogChoice

--- UlrikHovsgaardAlgorithm/UlrikHovsgaardWpf/ViewModels/SelectActorWindowViewModel.cs
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardWpf/ViewModels/SelectActorWindowViewModel.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace UlrikHovsgaardWpf.ViewModels
 {
     public class SelectActorWindowViewModel : SuperViewModel
     {
+        #region Properties
+
+        private string _activityAmountUpperBound;
+
+        public string ActivityAmountUpperBound { get { return _activityAmountUpperBound; } set { _activityAmountUpperBound = value; OnPropertyChanged(); } }
+
+        #region Commands
+
+
+        private ICommand _confirmUpperBoundSelectionCommand;
+
+        public ICommand ConfirmUpperBoundSelectionCommand { get { return _confirmUpperBoundSelectionCommand; } set { _confirmUpperBoundSelectionCommand = value; OnPropertyChanged(); } }
+
+        #endregion
+
+        #endregion
+
 
     }
 }
