92cf3210a8eccb0c5cbba5a6de376e4e491a3007
Kirluu <kirluu4thewin@gmail.com>
2017-09-05 09:45:59 +0200

    Upgraded .Net framework

---
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithm/RedundancyRemoval/RedundancyRemoverComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UlrikHovsgaardAlgorithm.Datamodels;
+
+namespace UlrikHovsgaardAlgorithm.RedundancyRemoval
+{
+    public class RedundancyRemoverComparer
+    {
+
+
+        public void PerformComparison(DcrGraphSimple dcr)
+        {
+            
+        }
+    }
+}
