932f7115eff267d0f94f459e7caef16beab0af4c
Kirluu <kirluu4thewin@gmail.com>
2017-09-08 12:11:25 +0200

    Added XML test case (Mortgage Application)

---
+++ UlrikHovsgaardAlgorithm/UlrikHovsgaardAlgorithmTests/RedundancyRemoval/RedundancyRemovalComparerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UlrikHovsgaardAlgorithmTests.RedundancyRemoval
+{
+    [TestClass()]
+    public class RedundancyRemovalComparerTests
+    {
+        [TestMethod()]
+        public void PrimaryComparison()
+        {
+            
+        }
+    }
+}
